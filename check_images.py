9ff874d375528ee3c061e0edac4886b56fd66a8f
Micaela K <micaela.kulesz@gmail.com>
2020-06-01 13:40:15 -0300

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -3,7 +3,7 @@
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
 # TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: Micaela Kulesz
+# PROGRAMMER: Micaela
 # DATE CREATED: 17.05.2020                                  
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
